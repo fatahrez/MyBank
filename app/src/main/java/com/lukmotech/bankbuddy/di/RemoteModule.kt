d34a51d81d302dbbb643543a8edc66d53d102a20
fatahrez <fertahrez@gmail.com>
2020-04-08 21:28:50 +0300

    Add logger fix API issue and push final code

--- app/src/main/java/com/lukmotech/bankbuddy/di/RemoteModule.kt
+++ app/src/main/java/com/lukmotech/bankbuddy/di/RemoteModule.kt
@@ -5,6 +5,8 @@ import com.lukmotech.bankbuddy.data.model.TransactionData
 import com.lukmotech.bankbuddy.data.model.UserInfoData
 import com.lukmotech.bankbuddy.data.repository.RemoteDataSource
 import com.lukmotech.remote.api.BankingService
+import com.lukmotech.remote.api.HttpClient
+import com.lukmotech.remote.api.HttpLogger
 import com.lukmotech.remote.mapper.Mapper
 import com.lukmotech.remote.mapper.TransactionDataNetworkMapper
 import com.lukmotech.remote.mapper.UserInfoDataNetworkMapper
@@ -14,9 +16,12 @@ import com.lukmotech.remote.source.RemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module(includes = [RemoteModule.Binders::class])
 class RemoteModule {
@@ -41,13 +46,27 @@ class RemoteModule {
     }
 
     @Provides
-    fun providesBankingService(retrofit: Retrofit): BankingService = retrofit.create(BankingService::class.java)
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor = HttpLogger.create()
 
     @Provides
-    fun providesRetrofit(): Retrofit =
-        Retrofit.Builder()
+    fun providersOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return HttpClient.setupOkHttpClient(httpLoggingInterceptor)
+    }
+
+    @Singleton
+    @Provides
+    fun providesBankingService(retrofit: Retrofit): BankingService = retrofit.create(BankingService::class.java)
+
+
+
+    @Provides
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .baseUrl(BuildConfig.BASE_URL)
             .build()
+    }
+
 }
---
+++ remote/src/main/java/com/lukmotech/remote/api/HttpLogger.kt
@@ -0,0 +1,9 @@
+package com.lukmotech.remote.api
+
+import okhttp3.logging.HttpLoggingInterceptor
+
+object HttpLogger {
+    fun create(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
